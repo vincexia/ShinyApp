ff36f83f04af3a5b35f632a3300e38f85bfdc1a1
Wenwen Xia <xiawensteady@gmail.com>
2022-06-06 21:32:06 +0800

    updated model2

--- server.R
+++ server.R
@@ -1,9 +1,9 @@
 ##
 library(shiny)
+library(ggplot2)
 shinyServer(function(input, output) {
-    mtcars$wtsp <- ifelse(mtcars$wt - 3.5 > 0, mtcars$wt - 3.5, 0)
     model1 <- lm(mpg ~ wt, data = mtcars)
-    model2 <- lm(mpg ~ wtsp + wt, data = mtcars)
+    model2 <- lm(mpg ~ wt + cyl, data = mtcars)
     
     model1pred <- reactive({
         wtInput <- input$sliderWeight
@@ -12,10 +12,10 @@ shinyServer(function(input, output) {
     
     model2pred <- reactive({
         wtInput <- input$sliderWeight
+        cylInput <- input$selectCylinder
         predict(model2, newdata = 
                     data.frame(wt = wtInput,
-                               wtsp = ifelse(wtInput - 3.5 > 0,
-                                              wtInput - 3.5, 0)))
+                               cyl = as.numeric(cylInput)))
     })
     
     output$plot1 <- renderPlot({
@@ -27,17 +27,22 @@ shinyServer(function(input, output) {
         if(input$showModel1){
             abline(model1, col = "red", lwd = 2)
         }
-        if(input$showModel2){
-            model2lines <- predict(model2, newdata = data.frame(
-                wt = 1.5:6, wtsp = ifelse(1.5:6 - 3.5 > 0, 1.5:6 - 3.5, 0)
-            ))
-            lines(1.5:6, model2lines, col = "blue", lwd = 2)
-        }
         
         legend("topright", c("Model 1 Prediction", "Model 2 Prediction"), pch = 16, 
                col = c("red", "blue"), bty = "n", cex = 1.2)
         points(wtInput, model1pred(), col = "red", pch = 16, cex = 2)
-        points(wtInput, model2pred(), col = "blue", pch = 16, cex = 2)
+    })
+    
+    output$plot2 <- renderPlot({
+        wtInput <- input$sliderWeight
+        
+        g <- ggplot(data = mtcars, aes(x=wt, y=mpg, group=cyl, color=as.factor(cyl))) +
+            geom_point()
+        g
+        if(input$showModel2){
+            g + geom_smooth(method = "lm") +
+                geom_point(aes(x=wtInput, y=model2pred()), colour="blue", size = 4)
+        }
     })
     
     output$pred1 <- renderText({
--- ui.R
+++ ui.R
@@ -3,10 +3,12 @@
 # load shiny package
 library(shiny)
 shinyUI(fluidPage(
-    titlePanel("Predict MPG from Weight"),
+    titlePanel("Predict MPG from Weight and Cylinder"),
     sidebarLayout(
         sidebarPanel(
             sliderInput("sliderWeight", "What is the weight of the car?", 1.5, 6, value = 3.5),
+            selectInput("selectCylinder", "Cylinder number:",
+                        c(4, 6, 8)),
             checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
             checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
             submitButton("Submit")
@@ -15,20 +17,20 @@ shinyUI(fluidPage(
             tabsetPanel(type = "tabs", 
                         tabPanel("Model Prediction", 
                                  plotOutput("plot1"),
+                                 plotOutput("plot2"),
                                  h3("Predicted MPG from Model 1:"),
                                  textOutput("pred1"),
                                  h3("Predicted MPG from Model 2:"),
                                  textOutput("pred2")), 
                         tabPanel("Documentation", h3("The Shiny application shows 
-                                                     the predictions of MPG based on the weight in mtcars
+                                                     the predictions of MPG based on the weight and cylinder in mtcars
                                                      dataset."),
                                  h3("The 1st model is the linear model of mpg over wt."),
-                                 h3("The 2nd model is the linear model of mpg over wt and wtsp, where wtsp 
-                                    takes the threshold value 3.5 in wt. If wt is larger than 3.5, wtsp is 
-                                    the difference below wt and 3.5, otherwise wtsp is zero. "),
+                                 h3("The 2nd model is the linear model of mpg over wt and cyl, where cyl 
+                                    is the number of cylinders with selection of value 4, 6 or 8. "),
                                  
-                                 h3("In general, the line for the 1st model is straight line, and the line 
-                                    for the 2nd model is a broken line at point wt = 3.5. ")) 
+                                 h3("In general, the line for the 1st model is straight line, and the 2nd model has 
+                                    one line for each cylinder selection.")) 
             )
         )
     )
